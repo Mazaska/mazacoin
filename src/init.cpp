4d809d6711ce2e24f7b31fdcd2f0ff9357995fcb
Kuwanlelenta <kuwanlelenta@blkid.net>
2018-12-29 00:39:32 -0700

    Mazacoin: Rename IPC Threads

--- src/init.cpp
+++ src/init.cpp
@@ -185,7 +185,7 @@ void Shutdown()
     /// for example if the data directory was found to be locked.
     /// Be sure that anything that writes files or flushes caches only does this if the respective
     /// module was initialized.
-    RenameThread("bitcoin-shutoff");
+    RenameThread("mazacoin-shutoff");
     mempool.AddTransactionsUpdated(1);
 
     StopHTTPRPC();
@@ -569,7 +569,7 @@ void CleanupBlockRevFiles()
 void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
 {
     const CChainParams& chainparams = Params();
-    RenameThread("bitcoin-loadblk");
+    RenameThread("mazacoin-loadblk");
     CImportingNow imp;
 
     // -reindex
